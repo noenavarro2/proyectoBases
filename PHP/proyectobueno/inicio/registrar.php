bab7bf934fd153092fc5b13a7010eebf3ba19bf0
sergiosanz <sanzestradasergio@gmail.com>
2017-05-16 09:16:39 +0200

    Subiendo los cambios de index y estilos

--- PHP/proyectobueno/inicio/registrar.php
+++ PHP/proyectobueno/inicio/registrar.php
@@ -1,17 +1,25 @@
 <?php
 //Recuperamos datos del formulario
 $tiu1=$_POST['cbox1'];
+//$tiu2=$_POST['cbox2'];
+//$tiu3=$_POST['cbox3'];
 $nom=$_POST['nombrere'];
 $dni=$_POST['usuariore'];
 $cla=$_POST['clavere'];
 $fen=$_POST['fechare'];
-
+if($tiu1==0){
+	$tuse = "Profesor";
+}
+if($tiu1==1){
+	$tuse = "Alumno";
+}
+if($tiu1==2){
+	$tuse= "SAT";
+}
 //conectamos con la BD
 include("conexion.php");
 //creamos consulta
-
-	$sql="INSERT INTO usuarios(nombre,dni,clave,fechaNacimiento,tipoUsuario) VALUES ('$nom','$dni','$cla','$fen','$tiu1')";
-
+$sql="INSERT INTO usuarios(tipoUsuario, nombre,dni,clave,fechaNacimiento) VALUES ('$tuse','$nom','$dni','$cla','$fen')";
 //ejecutamos la consulta
 mysqli_query($conexion,$sql) or die("Error en la
 consulta de inserción $sql");
